317b3b39065121a2da78f4a85183e8ae5242056a
congyaozhu <congyaozhu@ebnew.com>
2019-09-11 14:47:58 +0800

    新建待删除分支

---
+++ src/test/java/cn/bidlink/report/app/TestMain.java
@@ -0,0 +1,14 @@
+package cn.bidlink.report.app;
+
+/**
+ * @author ：congyaozhu
+ * @date ：Created in 2019/9/10 14:12
+ * @description：
+ * @version: $
+ */
+public class TestMain {
+    public static void main(String[] args){
+//        Date date = DateUtils.parseDate("2018-04-28 18:54:10", "yyyy-MM-dd HH:mm:ss");
+        System.out.println(123);
+    }
+}
