62f59e8a23256b532fe4651a317cf68833ed00ee
congyaozhu <congyaozhu@ebnew.com>
2019-09-23 09:16:52 +0800

    移动 dashboard程序集模板 到 dashboard目录

--- src/main/java/cn/bidlink/report/app/datasource/dashboard/DashboardSupplierDealPriceTrend.java
+++ src/main/java/cn/bidlink/report/app/datasource/dashboard/DashboardSupplierDealPriceTrend.java
@@ -1,4 +1,4 @@
-package cn.bidlink.report.app.datasource.purchase;
+package cn.bidlink.report.app.datasource.dashboard;
 
 import cn.bidlink.report.app.datasource.abstracts.AbstractBaseTableData;
 import cn.bidlink.report.app.utils.DataServiceFactory;
@@ -14,30 +14,30 @@ import java.util.*;
  * @Date 2019/9/17 19:50
  * @Version 1.0
  **/
-public class DashboardItemDealPriceTrend extends AbstractBaseTableData {
+public class DashboardSupplierDealPriceTrend extends AbstractBaseTableData {
 
     @Override
     protected Parameter[] getParameter() {
         return new Parameter[]{
-                new Parameter("directoryId")
+                new Parameter("supplierId")
         };
     }
 
     @Override
     protected String[] getColumn() {
-        return new String[]{"directoryId", "price", "month"};
+        return new String[]{"supplierId", "price", "month"};
     }
 
     @Override
     protected List getQueryData(DataServiceFactory dataServiceFactory, Map<String, String> param) {
-        Long directoryId = Long.valueOf( StringUtils.isEmpty(param.get("directoryId")) ? "0" : param.get("directoryId"));
+        Long supplierId = Long.valueOf(StringUtils.isEmpty(param.get("supplierId")) ? "0" : param.get("supplierId"));
         List<Map<String,Object>> list = new ArrayList<>();
         for (int i = 1 ; i <= 12 ; i ++){
             Map<String,Object> map = new HashMap<>();
-            map.put("directoryId",directoryId);
+            map.put("supplierId",supplierId);
             map.put("month",i);
-            int max= 80;
-            int min = 50;
+            int max= 30;
+            int min = 10;
             map.put("price",new Random().nextInt(max-min+1)+min);
             list.add(map);
         }
