405303574961bef3551a8c7bc094cd9a679d4224
xinyuli <xinyuli@ebnew.com>
2019-09-25 15:57:09 +0800

    采购项目：成交结果采购品-供应商维度数据集

---
+++ src/main/java/cn/bidlink/report/app/datasource/purchase/PurchaseClinchDealResultPurProductDataSource.java
@@ -0,0 +1,54 @@
+
+package cn.bidlink.report.app.datasource.purchase;
+
+import cn.bidlink.framework.boot.web.context.UserContext;
+import cn.bidlink.procurement.purchase.cloud.vo.DealItemSupplierVo;
+import cn.bidlink.report.app.datasource.abstracts.AbstractBaseTableData;
+import cn.bidlink.report.app.model.vo.purchase.ClinchDealVo;
+import cn.bidlink.report.app.service.PurchaseProxyService;
+import cn.bidlink.report.app.utils.DataServiceFactory;
+import com.fr.base.Parameter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:xinyuli@ebnew.com">wisdom</a>
+ * @version Ver 1.0
+ * @description:采购项目：成交结果采购品-供应商维度 采购品数据集
+ * @Date 2019/9/24
+ *
+ */
+public class PurchaseClinchDealResultPurProductDataSource extends AbstractBaseTableData {
+
+    @Override
+    protected Parameter[] getParameter() {
+        return new Parameter[]{
+                new Parameter("projectId")
+        };
+    }
+
+    @Override
+    protected String[] getColumn() {
+        return new String[]{"projectItemId" ,"directoryId" , "name", "spec", "purchaseAmount","unitName", "marketPrice", "planPrice"};
+    }
+
+    @Override
+    protected List getQueryData(DataServiceFactory dataServiceFactory, Map<String, String> param) {
+
+        PurchaseProxyService purchaseProxyService = dataServiceFactory.getDataService(PurchaseProxyService.class);
+        DealItemSupplierVo itemSupplierVoList = purchaseProxyService.quotationPricing(Long.valueOf(param.get("projectId")),
+                UserContext.getCompanyId(), UserContext.getUserId(), 3, false);
+        List<Map<String, Object>> tableData = itemSupplierVoList.getTableData();
+
+        List<ClinchDealVo> tableDatanull = new ArrayList<>();
+        if (null != tableData && tableData.size()>0){
+            return tableData;
+        }else {
+            return tableDatanull;
+        }
+
+    }
+
+}
