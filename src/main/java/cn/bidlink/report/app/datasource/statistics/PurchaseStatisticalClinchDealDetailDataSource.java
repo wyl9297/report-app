10aaa95a0c7a40418823f64d471fe730e23d6ea2
xinyuli <xinyuli@ebnew.com>
2019-10-16 08:50:53 +0800

    采购项目 采购成交明细报表数据量过大问题

--- src/main/java/cn/bidlink/report/app/datasource/statistics/PurchaseStatisticalClinchDealDetailDataSource.java
+++ src/main/java/cn/bidlink/report/app/datasource/statistics/PurchaseStatisticalClinchDealDetailDataSource.java
@@ -8,10 +8,11 @@ import cn.bidlink.report.server.service.PurchaseStatisticalDealDetailService;
 import com.fr.base.Parameter;
 import com.fr.third.org.apache.commons.lang3.time.DateUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @author <a href="mailto:xinyuli@ebnew.com">wisdom</a>
@@ -21,6 +22,9 @@ import java.util.Map;
  *
  */
 public class PurchaseStatisticalClinchDealDetailDataSource extends AbstractBaseTableData {
+
+    private Logger logger = LoggerFactory.getLogger(PurchaseStatisticalClinchDealDetailDataSource.class);
+
     @Override
     protected Parameter[] getParameter() {
         return new Parameter[]{
@@ -41,25 +45,64 @@ public class PurchaseStatisticalClinchDealDetailDataSource extends AbstractBaseT
 
     @Override
     protected List getQueryData(DataServiceFactory dataServiceFactory, Map<String, String> param) {
-        PurchaseStatisticalDealDetailService purchaseStatisticalDealDetailService = dataServiceFactory.getDataService(PurchaseStatisticalDealDetailService.class);
-        PurchaseNewProjectWideTable purchaseNewProjectWideTable = new PurchaseNewProjectWideTable();
-        purchaseNewProjectWideTable.setCompanyId(Long.valueOf(param.get("companyId")));
-        purchaseNewProjectWideTable.setDepartmentName(param.get("departmentName"));
-        purchaseNewProjectWideTable.setCatalogNamePath(param.get("catalogNamePath"));
-        try {
-            purchaseNewProjectWideTable.setStartCreateTime(StringUtils.isNotEmpty(param.get("startCreateTime"))?DateUtils.parseDate(param.get("startCreateTime"), "yyyy-MM-dd HH:mm:ss"):null);
-            purchaseNewProjectWideTable.setEndCreateTime(StringUtils.isNotEmpty(param.get("endCreateTime"))?DateUtils.parseDate(param.get("endCreateTime"), "yyyy-MM-dd HH:mm:ss"):null);
-            purchaseNewProjectWideTable.setStartArchiveTime(StringUtils.isNotEmpty(param.get("startArchiveTime"))?DateUtils.parseDate(param.get("startArchiveTime"), "yyyy-MM-dd HH:mm:ss"):null);
-            purchaseNewProjectWideTable.setEndArchiveTime(StringUtils.isNotEmpty(param.get("endArchiveTime"))?DateUtils.parseDate(param.get("endArchiveTime"), "yyyy-MM-dd HH:mm:ss"):null);
-        }catch (Exception e){
-            e.printStackTrace();
+        List<PurchaseNewProjectWideTable> resultCheck = new ArrayList<>();
+        String startCreateTime = param.get("startCreateTime");
+        String endCreateTime = param.get("endCreateTime");
+        String startArchiveTime = param.get("startArchiveTime");
+        String endArchiveTime = param.get("endArchiveTime");
+
+        Boolean flag = true;
+        if (StringUtils.isNotBlank(startCreateTime) && StringUtils.isNotBlank(endCreateTime)){
+            flag =true;
+        }else if (StringUtils.isNotBlank(startArchiveTime) && StringUtils.isNotBlank(endArchiveTime)){
+            flag =true;
+        }else if (StringUtils.isBlank(startCreateTime) || StringUtils.isBlank(endCreateTime)){
+            flag =false;
+        }else if (StringUtils.isBlank(startArchiveTime) || StringUtils.isBlank(endArchiveTime)){
+            flag =false;
         }
-        ServiceResult<List<PurchaseNewProjectWideTable>> purchaseDealStatistical = purchaseStatisticalDealDetailService.findPurchaseStatisticalDealDetail(purchaseNewProjectWideTable);
-        List<PurchaseNewProjectWideTable> result = purchaseDealStatistical.getResult();
-        if (result != null && result.size()>0){
-            return result;
+
+        if (flag){
+            PurchaseStatisticalDealDetailService purchaseStatisticalDealDetailService = dataServiceFactory.getDataService(PurchaseStatisticalDealDetailService.class);
+            PurchaseNewProjectWideTable purchaseNewProjectWideTable = new PurchaseNewProjectWideTable();
+            purchaseNewProjectWideTable.setCompanyId(Long.valueOf(param.get("companyId")));
+            purchaseNewProjectWideTable.setDepartmentName(param.get("departmentName"));
+            purchaseNewProjectWideTable.setCatalogNamePath(param.get("catalogNamePath"));
+            try {
+                long l = System.currentTimeMillis();
+                Date datetim = new Date(l);
+                SimpleDateFormat dateStartDefault = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String format = dateStartDefault.format(datetim);
+                Date dateStartCreateTime = DateUtils.parseDate(format, "yyyy-MM-dd HH:mm:ss");
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.MONTH, -1);
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH)+1;
+                int date = calendar.get(Calendar.DATE);
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+                int second = calendar.get(Calendar.SECOND);
+                String dateEndDefault = year+"-"+(month<10?"0"+month:month)+"-"+(date<10?"0"+date:date)+" "+(hour<10?"0"+hour:hour)+":"+(minute<10?"0"+minute:minute)+":"+(second<10?"0"+second:second);
+                Date dateEndCreateTime = DateUtils.parseDate(dateEndDefault, "yyyy-MM-dd HH:mm:ss");
+
+                purchaseNewProjectWideTable.setStartCreateTime(StringUtils.isNotEmpty(param.get("startCreateTime"))?DateUtils.parseDate(param.get("startCreateTime"), "yyyy-MM-dd HH:mm:ss"):dateStartCreateTime);
+                purchaseNewProjectWideTable.setEndCreateTime(StringUtils.isNotEmpty(param.get("endCreateTime"))?DateUtils.parseDate(param.get("endCreateTime"), "yyyy-MM-dd HH:mm:ss"):dateEndCreateTime);
+                purchaseNewProjectWideTable.setStartArchiveTime(StringUtils.isNotEmpty(param.get("startArchiveTime"))?DateUtils.parseDate(param.get("startArchiveTime"), "yyyy-MM-dd HH:mm:ss"):null);
+                purchaseNewProjectWideTable.setEndArchiveTime(StringUtils.isNotEmpty(param.get("endArchiveTime"))?DateUtils.parseDate(param.get("endArchiveTime"), "yyyy-MM-dd HH:mm:ss"):null);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            ServiceResult<List<PurchaseNewProjectWideTable>> purchaseDealStatistical = purchaseStatisticalDealDetailService.findPurchaseStatisticalDealDetail(purchaseNewProjectWideTable);
+            List<PurchaseNewProjectWideTable> result = purchaseDealStatistical.getResult();
+            if (result != null && result.size()>0){
+                return result;
+            }
+            List<PurchaseNewProjectWideTable> resultNull = new ArrayList<>();
+            return resultNull;
+        }else {
+            throw new RuntimeException("您好,请输入立项开始时间和结束时间或者归档开始时间和结束时间");
         }
-        List<PurchaseNewProjectWideTable> resultNull = new ArrayList<>();
-        return resultNull;
+
     }
 }
