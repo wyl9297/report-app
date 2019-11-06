21f6ccb69efdc7c6cb13ba91b943ad290659e56f
赵阳 <zhaoyang01@ebnew.com>
2019-11-06 17:42:45 +0800

    使用随机数，代替固定内容

--- src/main/java/cn/bidlink/report/app/datasource/nyc/InsertParam.java
+++ src/main/java/cn/bidlink/report/app/datasource/nyc/InsertParam.java
@@ -1,16 +1,14 @@
 package cn.bidlink.report.app.datasource.nyc;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class InsertParam {
     public static List insert(String[] strings) {
         List<Map<String, Object>> resultList = new ArrayList<>();
+        Random random = new Random();
         Map<String, Object> resultMap = new HashMap<>();
         for (int i = 0; i < strings.length; i++) {
-            resultMap.put(strings[i], 555);
+            resultMap.put(strings[i],  random.nextInt());
         }
         resultList.add(resultMap);
         return resultList;
